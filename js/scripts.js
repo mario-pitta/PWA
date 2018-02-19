f033969d063ac3dfc562eee0eba4cf9b08a7222e
mario-pitta <pitta_mario@hotmail.com>
2018-02-19 15:13:55 -0300

    eventos criados

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,88 @@
 let notes = { data: [] };
 
+let updateList = function (){
+    console.log('[App] start watch');
+
+    Array.observe(notes.data, function (changes){
+        let index = null;
+        let value = '';
+        let status = null;
+
+        if (changes[0].type === 'splice'){
+            index = changes[0].index;
+            value = changes[0].object[index];
+            status = (changes[0].addedCount > 0) ? 'created' : 'removed';
+        }
+
+        if (changes[0].type === 'update'){
+            index = changes[0].name;
+            value = changes[0].object[index];
+            status = 'updated';
+        }
+
+        if (!value && status === 'created' && status === 'updated'){
+            return;
+        }
+
+        let notesTag = document.getElementById('notes');
+
+        if (status === 'updated'){
+            console.log('implementar')
+        }
+        if (status === 'removed'){
+            let listOfNotes = document.querySelectorAll("#notes li");
+            notesTag.removeChild(listOfNotes[index]);
+        }
+        if (status === 'created'){
+            let newLi = document.createElement('li');
+            newLi.innerHTML = value;
+            notesTag.appendChild(newLi);
+        }
+    });
+}
 
 let createNote = function() {
     let input = document.querySelector('#form-add-note input[type="text"]');
-    let value = input.value;
-    if (value != null) {
-        notes.data.push(value);
+    let value = input.value
 
-        console.log(notes)
-
-        input.value = "";
-    } else {
-        alert('Campo Vazio');
-    }
+    notes.data.push(value);
+    console.log('ok');
+    console.log(value);
+    input.value = "";
 }
 
+updateList();
+
+
 document.addEventListener('DOMContentLoaded', function(event) {
+    let listOfNotes = document.getElementById('notes');
+    let listHtml = '';
+
+    for(let i=0; i < notes.data.length; i++){
+        listHtml += '<li>'+ notes.data[i] + '</li>'
+    }
+
+    listOfNotes.innerHTML = listHtml;
+
     let sendNote = document.getElementById('form-add-note')
     sendNote.addEventListener('submit', function(e) {
         e.preventDefault();
+        console.log("enviou ");
         createNote();
     });
+})
+
+document.addEventListener('click', function(e) {
+    let notesTag =  document.getElementById('notes');
+
+    if(e.target.parentElement === notesTag){
+        if (confirm('remover essa nota?')){
+            let listOfNotes = document.querySelectorAll('#notes li');
+            listOfNotes.forEach(function (item, index){
+                if (e.target === item){
+                    notes.data.splice(index, 1);
+                }
+            });
+        }
+    }
 })
